4b11763179c89f15977a9b0a621ac0b0ea432324
nguyenanhung <info@nguyenanhung.com>
2023-02-14 16:24:04 +0700

    Update

--- helpers/misc.php
+++ helpers/misc.php
@@ -1,28 +1,36 @@
 <?php
-if (!function_exists('debug_pre_print_r')) {
-    function debug_pre_print_r($str)
+if (!function_exists('_debug_pre_print_r_')) {
+    function _debug_pre_print_r_($var)
     {
         echo "<pre>";
-        if (is_array($str)) {
-            print_r($str);
+        if (is_array($var)) {
+            print_r($var);
         } else {
-            var_dump($str);
+            var_dump($var);
         }
         echo "</pre>";
     }
 }
-if (!function_exists('debug_vardump_pre')) {
-    function debug_vardump_pre($str = '')
+if (!function_exists('_debug_var_dump_pre_')) {
+    function _debug_var_dump_pre_($var)
     {
         echo "<pre>";
-        var_dump($str);
+        var_dump($var);
         echo "</pre>";
     }
 }
-if (!function_exists('debug_pre_print_r_die')) {
-    function debug_pre_print_r_die($str = '')
+if (!function_exists('_debug_pre_print_r_die_')) {
+    function _debug_pre_print_r_die_($var)
     {
-        debug_pre_print_r($str);
+        _debug_pre_print_r_($var);
         die;
+    }
+}
+if (!function_exists('_debug_var_export_')) {
+    function _debug_var_export_($var)
+    {
+        echo "<pre>";
+        var_export($var);
+        echo "</pre>";
     }
 }
